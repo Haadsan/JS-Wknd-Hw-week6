acaf8e90cde34457e770a5a215d664ffbfd615c3
Hadsan Geele <hgeele22@gmail.com>
2018-10-14 14:54:57 +0100

    Add delete button

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@
 document.addEventListener('DOMContentLoad', () => {
   const newItemform = document.querySelector("#new-item-form");
   newItemform.addEventListener('submit', handleNewItemFormSubmit);
+
+  const deleteAllButton = document.querySelector('#delete-all');
+  deleteAllButton.addEventListener('click', handleDeleteAllButton);
 })
 
 const handleNewItemFormSubmit = function (event) {
@@ -10,30 +13,25 @@ const handleNewItemFormSubmit = function (event) {
 // console.log(event);
  // console.dir(event);
 
-
   const dinosaurListItem = createDinosaurListItem(event.target);
   const dinosaurList = document.querySelector('#dinosaur-list');
   dinosaurList.appendChild(dinosaurListItem);
   // console.log(event);
    // console.dir(event);
-
   event.target.reset();
 }
-
-
 // CREATE
 const createDinosaurListItem = function (form) {
   const dinosaurListItem = document.createElement('li');
   dinosaurListItem.classList.add('dinosaur-list-item');
 
-  const dinasaurName = document.createElement('h2');
-  dinasaurName.textContent = form.dinasaurName.value;
+  const dinosaurName = document.createElement('h2');
+  dinosaurName.textContent = form.dinosaurName.value;
   dinosaurListItem.appendChild(dinosaurName);
 
   const diet = document.createElement('p');
   diet.textContent = form.diet.value;
   dinosaurListItem.appendChild(diet);
-  console.log(form.diet.value);
 
   const breed = document.createElement('p');
   breed.textContent = form.diet.value;
@@ -43,6 +41,10 @@ const createDinosaurListItem = function (form) {
   continent.textContent = form.continent.value;
   dinosaurListItem.appendChild(continent);
 
-
   return dinosaurListItem;
+}
+
+const handleDeleteAllClick = function (event) {
+  const dinosaurList = document.querySelector('#dinosaur-list');
+  dinosaurList.innerHTML = '';
 }
