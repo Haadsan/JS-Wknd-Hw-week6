463348d447dc37517fc0dac18c678598d586949a
Hadsan Geele <hgeele22@gmail.com>
2018-10-15 11:32:08 +0100

    updated hmw

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 //  load the form first
-document.addEventListener('DOMContentLoad', () => {
+document.addEventListener('DOMContentLoaded', () => {
+  // console.log('loaded')
+  // hook to form
   const newItemform = document.querySelector("#new-item-form");
   newItemform.addEventListener('submit', handleNewItemFormSubmit);
+
+  // this will listen for the form submission and not the button
+  // mdn event https://developer.mozilla.org/en-US/docs/Web/Events
 
   const deleteAllButton = document.querySelector('#delete-all');
   deleteAllButton.addEventListener('click', handleDeleteAllButton);
@@ -9,19 +14,25 @@ document.addEventListener('DOMContentLoad', () => {
 
 const handleNewItemFormSubmit = function (event) {
   event.preventDefault();
-// console.log(event);
- // console.dir(event);
-  const dinosaurListItem = createDinosaurListItem();
+  // console.log(event);
+  // console.dir(event);
+  // event.target give us our form
+
+  const dinosaurListItem = createDinosaurListItem(event);
+  // grab
   const dinosaurList = document.querySelector('#dinosaur-list');
   dinosaurList.appendChild(dinosaurListItem);
   // console.log(event);
-   // console.dir(event);
+  // console.dir(event);
   event.target.reset();
 }
 // CREATE/Write
+// debugger;
 const createDinosaurListItem = function (event) {
   const dinosaurListItem = document.createElement('li');
   dinosaurListItem.classList.add('dinosaur-list-item');
+  // this will add to the list of class for styling css
+
 
   const dinosaurName = document.createElement('h2');
   dinosaurName.textContent = event.target.dinosaurName.value;
@@ -30,6 +41,7 @@ const createDinosaurListItem = function (event) {
   const diet = document.createElement('p');
   diet.textContent = event.target.diet.value;
   dinosaurListItem.appendChild(diet);
+
 
   const breed = document.createElement('p');
   breed.textContent = event.target.diet.value;
@@ -42,7 +54,12 @@ const createDinosaurListItem = function (event) {
   return dinosaurListItem;
 }
 
-const handleDeleteAllClick = function (event) {
+const handleDeleteAllButton = function (event) {
   const dinosaurList = document.querySelector('#dinosaur-list');
   dinosaurList.innerHTML = '';
 }
+
+//     const img = document.createElement("img");
+// img.src = "/my_image.jpg";
+// img.alt = "My alt tag";
+// img.id = "someId";
